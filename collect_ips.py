078b15c2ad5ad2a0e4bcff1f93ca1cd10bc6d260
DKluo <lqxkjy@outlook.com>
2025-06-11 22:17:59 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -4,7 +4,8 @@ import re
 import os
 
 # 目标URL列表
-urls = ['https://api.uouin.com/cloudflare.html','https://ip.164746.xyz' ]
+urls = ['https://ip.164746.xyz' ]
+#'https://api.uouin.com/cloudflare.html',
 
 # 正则表达式用于匹配IP地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
